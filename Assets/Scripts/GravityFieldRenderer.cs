da7d836b71c99505cbd563c4a714b6e46303c1a5
LEPINE Quentin <q.lepine@student.isartdigital.com>
2023-03-13 16:17:54 +0100

    added more gravity field visualizers

--- Assets/Scripts/GravityFieldRenderer.cs
+++ Assets/Scripts/GravityFieldRenderer.cs
@@ -6,8 +6,9 @@ using Assets.Scripts;
 public enum RenderType : ushort
 {
     Grid2D = 0,
-    Grid3D = 1,
-    Arrow3D = 2
+    Arrow2D,
+    Grid3D,
+    Arrow3D
 }
 
 public class GravityFieldRenderer : MonoBehaviour
@@ -33,10 +34,24 @@ public class GravityFieldRenderer : MonoBehaviour
 
     void LateUpdate()
     {
-        if (!ShouldUpdateGrid) return;
+        if (!ShouldUpdateGrid || PointCount < 2) return;
         Vector3[] grid;
         int[] indexes;
-        Create2DGrid(out grid, out indexes);
+        switch (type)
+        {
+            case RenderType.Grid2D:
+                Create2DGrid(out grid, out indexes);
+                break;
+            case RenderType.Arrow2D:
+                Create2DVec(out grid, out indexes);
+                break;
+            case RenderType.Grid3D:
+                Create3DGrid(out grid, out indexes);
+                break;
+            default:
+                Create3DVec(out grid, out indexes);
+                break;
+        }
         mesh.SetIndices(null, MeshTopology.Lines, 0);
         mesh.SetVertices(grid);
         mesh.SetIndices(indexes, MeshTopology.Lines, 0);
@@ -51,11 +66,11 @@ public class GravityFieldRenderer : MonoBehaviour
     private void Create2DGrid(out Vector3[] grid, out int[] indexes)
     {
         grid = new Vector3[PointCount * PointCount];
-        indexes = new int[(PointCount - 1) * (PointCount - 1) * 4 + PointCount * 4];
+        indexes = new int[(PointCount - 1) * (PointCount - 1) * 4 + (PointCount - 1) * 4];
         int counter = 0;
-        for (int i = 0; i < PointCount; i++)
+        for (int i = 0; i < PointCount; ++i)
         {
-            for (int j = 0; j < PointCount; j++)
+            for (int j = 0; j < PointCount; ++j)
             {
                 int index = i * PointCount + j;
                 grid[index] = new Vector3((i * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize, 0, (j * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize);
@@ -80,27 +95,82 @@ public class GravityFieldRenderer : MonoBehaviour
     private void Create3DGrid(out Vector3[] grid, out int[] indexes)
     {
         grid = new Vector3[PointCount * PointCount * PointCount];
-        indexes = new int[(PointCount - 1) * (PointCount - 1) * 24];
+        indexes = new int[(PointCount - 1) * (PointCount - 1) * (PointCount - 1) * 6 + (PointCount - 1) * (PointCount - 1) * 12 + (PointCount - 1) * 6];
         int counter = 0;
-        for (int i = 0; i < PointCount; i++)
+        for (int i = 0; i < PointCount; ++i)
         {
-            for (int j = 0; j < PointCount; j++)
+            for (int j = 0; j < PointCount; ++j)
             {
-                int index = i * PointCount + j;
-                grid[index] = new Vector3((i * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize, 0, (j * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize);
-                double force = mPlanetDataManager.GetAccelForceAtPoint(mPlanetDataManager.GetFocusLerped() + new DVec3(grid[index]));
-                grid[index].y = (float)(-force * ScaleParameter);
-                if (i != 0 && j != 0)
+                for (int k = 0; k < PointCount; ++k)
                 {
-                    indexes[counter] = i * PointCount + j;
-                    indexes[counter + 1] = (i - 1) * PointCount + j;
-                    indexes[counter + 2] = (i - 1) * PointCount + j;
-                    indexes[counter + 3] = (i - 1) * PointCount + j - 1;
-                    indexes[counter + 4] = (i - 1) * PointCount + j - 1;
-                    indexes[counter + 5] = i * PointCount + j - 1;
-                    indexes[counter + 6] = i * PointCount + j - 1;
-                    indexes[counter + 7] = i * PointCount + j;
-                    counter += 8;
+                    int index = (i * PointCount + j) * PointCount + k;
+                    grid[index] = new Vector3((i * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize, (j * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize, (k * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize);
+                    DVec3 force = mPlanetDataManager.GetAccelAtPoint(mPlanetDataManager.GetFocusLerped() + new DVec3(grid[index])) * ScaleParameter;
+                    if (force.Length() > PointSize / PointCount) force = force.Normalized() * (PointSize / (PointCount - 1));
+                    grid[index] = grid[index] + force.AsVector();
+                    if (i != PointCount - 1)
+                    {
+                        indexes[counter] = (i * PointCount + j) * PointCount + k;
+                        indexes[counter + 1] = ((i + 1) * PointCount + j) * PointCount + k;
+                        counter += 2;
+                    }
+                    if (j != PointCount - 1)
+                    {
+                        indexes[counter] = (i * PointCount + j) * PointCount + k;
+                        indexes[counter + 1] = (i * PointCount + j + 1) * PointCount + k;
+                        counter += 2;
+                    }
+                    if (k != PointCount - 1)
+                    {
+                        indexes[counter] = (i * PointCount + j) * PointCount + k;
+                        indexes[counter + 1] = (i * PointCount + j) * PointCount + k + 1;
+                        counter += 2;
+                    }
+                }
+            }
+        }
+    }
+
+    private void Create2DVec(out Vector3[] grid, out int[] indexes)
+    {
+        grid = new Vector3[PointCount * PointCount * 2];
+        indexes = new int[PointCount * PointCount * 2];
+        int counter = 0;
+        for (int i = 0; i < PointCount; ++i)
+        {
+            for (int j = 0; j < PointCount; ++j)
+            {
+                int index = (i * PointCount + j) * 2;
+                grid[index] = new Vector3((i * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize, 0, (j * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize);
+                DVec3 force = mPlanetDataManager.GetAccelAtPoint(mPlanetDataManager.GetFocusLerped() + new DVec3(grid[index])) * ScaleParameter;
+                if (force.Length() > PointSize / PointCount) force = force.Normalized() * (PointSize / (PointCount - 1));
+                grid[index + 1] = grid[index] + force.AsVector();
+                indexes[counter] = (i * PointCount + j) * 2;
+                indexes[counter + 1] = (i * PointCount + j) * 2 + 1;
+                counter += 2;
+            }
+        }
+    }
+
+    private void Create3DVec(out Vector3[] grid, out int[] indexes)
+    {
+        grid = new Vector3[PointCount * PointCount * PointCount * 2];
+        indexes = new int[PointCount * PointCount * PointCount * 2];
+        int counter = 0;
+        for (int i = 0; i < PointCount; ++i)
+        {
+            for (int j = 0; j < PointCount; ++j)
+            {
+                for (int k = 0; k < PointCount; ++k)
+                {
+                    int index = ((i * PointCount + j) * PointCount + k) * 2;
+                    grid[index] = new Vector3((i * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize, (j * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize, (k * 1.0f / (PointCount - 1) - 0.5f) * (float)PointSize);
+                    DVec3 force = mPlanetDataManager.GetAccelAtPoint(mPlanetDataManager.GetFocusLerped() + new DVec3(grid[index])) * ScaleParameter;
+                    if (force.Length() > PointSize / PointCount) force = force.Normalized() * (PointSize / (PointCount - 1));
+                    grid[index + 1] = grid[index] + force.AsVector();
+                    indexes[counter] = ((i * PointCount + j) * PointCount + k) * 2;
+                    indexes[counter + 1] = ((i * PointCount + j) * PointCount + k) * 2 + 1;
+                    counter += 2;
                 }
             }
         }
